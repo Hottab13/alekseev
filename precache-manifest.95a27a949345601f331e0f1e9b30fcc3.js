e60f132ad38316fe57fc7cc043acf7f941cf7c27
Alekseev Aleksey <alekseev.a@hotmail.com>
2020-11-17 17:07:23 +0300

    Updates

---
+++ precache-manifest.95a27a949345601f331e0f1e9b30fcc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be20d4429ad4df8379c8ff893f05704",
+    "url": "/alekseev/index.html"
+  },
+  {
+    "revision": "f847b972e9ef0e9301ca",
+    "url": "/alekseev/static/css/2.969b5337.chunk.css"
+  },
+  {
+    "revision": "e1dfbd481143d377fc5f",
+    "url": "/alekseev/static/css/main.dbd19a5d.chunk.css"
+  },
+  {
+    "revision": "f847b972e9ef0e9301ca",
+    "url": "/alekseev/static/js/2.df06fbf9.chunk.js"
+  },
+  {
+    "revision": "53dbf85c1aac2b7ec840d6ec6fda98a2",
+    "url": "/alekseev/static/js/2.df06fbf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1dfbd481143d377fc5f",
+    "url": "/alekseev/static/js/main.fe7d40fa.chunk.js"
+  },
+  {
+    "revision": "1806f64103a6564ad72c",
+    "url": "/alekseev/static/js/runtime-main.0e55f77b.js"
+  },
+  {
+    "revision": "b4903b52b28339d2259149798d42017e",
+    "url": "/alekseev/static/media/banner-1.b4903b52.jpg"
+  },
+  {
+    "revision": "8b1a153c1719b049664e68c207d91de4",
+    "url": "/alekseev/static/media/banner-2.8b1a153c.jpg"
+  },
+  {
+    "revision": "110bb5436fc3d61c82d3aacb56b40121",
+    "url": "/alekseev/static/media/banner-3.110bb543.jpg"
+  },
+  {
+    "revision": "109b4cabcba1b3e0ca78fb8a2eb6c2cd",
+    "url": "/alekseev/static/media/banner-4.109b4cab.jpg"
+  }
+]);
